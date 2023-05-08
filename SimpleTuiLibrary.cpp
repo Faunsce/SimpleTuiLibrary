93320550a38f177a6a859a6264a2fb825d469d3a
Dominik Rutter <dominik.rutter@gmail.com>
2023-05-07 21:55:19 -0400

    Add project files.

---
+++ SimpleTuiLibrary.cpp
@@ -0,0 +1,12 @@
+﻿// SimpleTuiLibrary.cpp : Defines the entry point for the application.
+//
+
+#include "SimpleTuiLibrary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
